e0631b847a2b53602ec3ccc84cab8e5e963011f4
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-27 16:42:11 -0600

    Finished the movement of consulta general.

--- src/app/views/warehouse/page/pages/pages.component.ts
+++ src/app/views/warehouse/page/pages/pages.component.ts
@@ -1,3 +1,4 @@
+import { PageService } from 'app/services/page.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent implements OnInit {
+  pages = []
+  filtered = []
+  
+  constructor(private pageService: PageService) { 
+    this.pageService.all()
+      .subscribe( res => this.pages = this.filtered = res )      
+  }
 
-  constructor() { }
+  filter( value ) {
+    this.filtered = this.pages.filter( item => item.name.toLowerCase().includes(value.toLowerCase()))
+  }
 
   ngOnInit() {
   }
