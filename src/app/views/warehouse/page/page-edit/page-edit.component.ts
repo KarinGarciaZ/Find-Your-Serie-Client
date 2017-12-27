3ce09bbb2b3d6e817adae6a1a5cdcb98ef96d9d4
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-27 16:42:37 -0600

    Now it is possoble to edit a page.

--- src/app/views/warehouse/page/page-edit/page-edit.component.ts
+++ src/app/views/warehouse/page/page-edit/page-edit.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PageService } from 'app/services/page.service';
 import { Component, OnInit } from '@angular/core';
+import { Page } from 'app/models/page';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-page-edit',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-edit.component.scss']
 })
 export class PageEditComponent implements OnInit {
+  id
+  page: Page
 
-  constructor() { }
+  constructor(
+    private toastr: ToastrService,
+    private pageService: PageService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { 
+    this.activatedRoute.params.subscribe( params => {
+      this.id = params['id']
+      if ( this.id ) {
+        this.pageService.findById( this.id )
+          .subscribe( page => this.page = page )         
+      }
+    })
+  }
 
   ngOnInit() {
   }
 
+  onSubmitPage() {
+    this.pageService.update( this.page )
+      .subscribe( res => this.showSuccess(),
+      err => this.showError(err),
+      () => console.log('Done.')
+    )
+  }
+
+  showSuccess() {
+    this.toastr.success('Se editó exitosamente', '¡Registro editado!')
+  }
+
+  showError( err ) {
+    this.toastr.error( err.error.message, '¡Chanfle!' )
+  }
 }
